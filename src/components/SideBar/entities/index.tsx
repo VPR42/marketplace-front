7cc42b6a5d752eb568bd3067b052c4f1928448aa
Mokay93 <desterted@mail.ru>
2025-11-28 10:47:08 +0300

    добавил в сайдбар иконку профилей.

--- src/components/SideBar/entities/index.tsx
+++ src/components/SideBar/entities/index.tsx
@@ -1,9 +1,10 @@
-import { Home, LayoutDashboard, MessageCircle } from 'lucide-react';
+import { Home, LayoutDashboard, MessageCircle, UserCircle2 } from 'lucide-react';
 
 import type { NavItems } from '../types';
 
 export const NAV_ITEMS: NavItems = [
   { icon: Home, label: 'Главная', url: '/' },
   { icon: LayoutDashboard, label: 'Лента', url: '/feed' },
+  { icon: UserCircle2, label: 'Профиль', url: '/profile' },
   { icon: MessageCircle, label: 'Чаты', url: '/chats' },
 ];
