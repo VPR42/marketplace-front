1e9589762738ea870bd2ab81ce9b9029d32503f7
mayonezny <borischubuhchiev@mail.ru>
2025-11-29 14:41:14 +0300

    МОКИ МАРМОКИ

--- src/shared/data/services.ts
+++ src/shared/data/services.ts
@@ -1,6 +1,6 @@
 export const services = [
   {
-    id: 1,
+    id: '3fa85f64-5717-4562-b3fc-2c963f66afa6',
     title: 'Ремонт стиральных машин',
     description: 'Диагностика и ремонт всех видов стиральных машин.',
     price: '2000',
@@ -12,7 +12,7 @@ export const services = [
       'https://sun9-38.userapi.com/s/v1/ig2/O_n9fwqhc6ctMwJNzSoUqrtJf3Ux8g3blHJzygBp4VW6cmcHgPQ9mdBnjBUWjLTrXkt3v-SVjdFjWDdx5-4WuECr.jpg?quality=95&as=32x48,48x72,72x108,108x162,160x240,240x360,360x540,480x720,540x810,640x960,720x1080,1080x1620,1280x1920,1440x2160,1707x2560&from=bu&cs=1280x0',
   },
   {
-    id: 2,
+    id: '3a825f64-5717-4562-b3fc-2c963f66afa6',
     title: 'Генеральная уборка',
     description: 'Качественная уборка квартир и офисов.',
     price: '3500',
@@ -24,7 +24,7 @@ export const services = [
       'https://sun9-38.userapi.com/s/v1/ig2/O_n9fwqhc6ctMwJNzSoUqrtJf3Ux8g3blHJzygBp4VW6cmcHgPQ9mdBnjBUWjLTrXkt3v-SVjdFjWDdx5-4WuECr.jpg?quality=95&as=32x48,48x72,72x108,108x162,160x240,240x360,360x540,480x720,540x810,640x960,720x1080,1080x1620,1280x1920,1440x2160,1707x2560&from=bu&cs=1280x0',
   },
   {
-    id: 3,
+    id: '3fa85f64-5717-4362-b3fc-2c963f66afa6',
     title: 'Установка ПО',
     description: 'Установка и настройка программного обеспечения.',
     price: '1500',
